fcdb55b05de2fbb52b3bf50dc5c78772221fe07e
dzanotto <d.zanotto@gmail.com>
2024-12-02 10:34:52 +0100

    Added golden rule test

--- src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -10,7 +10,30 @@ public class GildedRoseApprovalTest {
 
     @Test
     public void testWithApprovals() {
-        String output = "This is a test!";
+        Item[] items = new Item[] {
+                new Item("Foo", 10, 50),
+                new Item("Bar", 5, 25),
+                new Item("Aged Brie", 10, 30),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5),
+                new Item( "test", 5, 0),
+                new Item("Norma", 7, 60),
+                new Item("Aged Brie", 10, 60),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 12, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 49)
+        };
+        GildedRose gildedRose = new GildedRose(items);
+        String output = "";
+        for (int j=0 ; j< 30; j++) {
+            gildedRose.updateQuality();
+            for (int i = 0; i < gildedRose.items.length; i++) {
+                output += gildedRose.items[i].toString() + "\n";
+            }
+        }
+
+
         Approvals.verify(output);
     }
 }
